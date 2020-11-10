26694cec46300de62db835a56c660a5d85bb77cf
Michael <wtfdic@hotmail.com>
2020-11-10 16:01:14 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -24,13 +24,13 @@ def valid_move?(board, index)
     end
   end
 # is position already taken and is valid
-def on_board?(num)
-  if num.between?(0, 8) == true
-    return true
-  else
-    return false
+  def on_board?(num)
+    if num.between?(0, 8) == true
+      return true
+    else
+      return false
+    end
   end
-end
 
   if (position_taken?(board, index)) == false && (on_board?(index) == true)
     return true
