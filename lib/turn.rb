4482eceba6e95511f7ee1125c18b93cbf318f6ab
Michael <wtfdic@hotmail.com>
2020-11-10 15:48:57 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -32,7 +32,7 @@ def valid_move?(board, index)
   end
 end
 
-  if (position_take?(board, index)) == false && (on_board?(index) == true)
+  if (position_taken?(board, index)) == false && (on_board?(index) == true)
     return true
   else
     return false
