821d32b314c6f18b44d9ead53ad4b545144260f7
Michael <wtfdic@hotmail.com>
2020-11-10 15:57:48 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -52,7 +52,7 @@ def turn(board)
   index = input_to_index(num)
   if valid_move?(board, index)  == true
     move(board, index)
-    display board
+    display_board(board)
   else
     turn(board)
 end
